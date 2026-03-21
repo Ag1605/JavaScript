cab4ffb19029eb4f1ca30e23a79007a2b9eceaab
Akshat <akshatgoyal96@gmail.com>
2026-03-21 21:08:11 +0530

    Initial commit

---
+++ string.js
@@ -0,0 +1,14 @@
+/* create a string */
+let str0 = "Hello";
+let str1= 'Hello';
+let name = "Akshat";
+let msg = `Hello ${name}`;
+console.log(msg);
+/* access characters in a string */
+let str = "Hello";
+
+console.log(str[0]); // H
+console.log(str.charAt(1)); // e
+
+
+
